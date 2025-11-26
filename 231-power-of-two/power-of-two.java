497f0e7ea9758ca068732a2401cc1639521c087c
lokendraff <lokendra.gla_cs23@gla.ac.in>
2025-11-26 16:26:02 +0530

    Time: 1 ms (54.46%) | Memory: 42.5 MB (24.55%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n<=0) return false;
-        int m = Math.abs(n);
-        return (m&(m-1))==0;
+        return (n&(n-1))==0;
     }
 }
